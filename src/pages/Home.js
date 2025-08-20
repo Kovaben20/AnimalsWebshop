25bfccdfa02b25105c79967f50986c546be607aa
Kovaben20 <129586198+Kovaben20@users.noreply.github.com>
2025-08-20 16:22:05 +0200

    Add Home component to the website

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Üdvözöl az Állatos Webshop!</h1>
+    <p>Böngéssz a termékeink között, vagy jelentkezz be a vásárláshoz.</p>
+  </div>
+);
+
+export default Home;
